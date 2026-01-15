c40711dde360546fc94c13620a203e3728fc596e
Jinny926 <qsdfg0926@gmail.com>
2026-01-15 17:29:44 +0900

    refactor: Memo Update 메모 수정 기능 구현

--- src/main/java/com/meta/memo/controller/MemoController.java
+++ src/main/java/com/meta/memo/controller/MemoController.java
@@ -10,7 +10,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static sun.security.pkcs11.wrapper.Functions.getId;
 
 @RestController
 @RequestMapping("api/memos")
