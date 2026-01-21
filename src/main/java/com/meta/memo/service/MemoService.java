bee01a2645b8d007347e4ae08c033df5370a9b10
Jinny926 <qsdfg0926@gmail.com>
2026-01-21 14:33:35 +0900

    refactor: Service 계층에서 트랜잭션 설정(CUD) / 읽기전용(R)

--- src/main/java/com/meta/memo/service/MemoService.java
+++ src/main/java/com/meta/memo/service/MemoService.java
@@ -4,95 +4,67 @@ import com.meta.memo.domain.Memo;
 import com.meta.memo.dto.MemoRequestDto;
 import com.meta.memo.dto.MemoResponseDto;
 import com.meta.memo.repository.MemoRepository;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+
 import java.util.List;
 
+@Service
 public class MemoService {
-    // JDBC를 통한 MySQL 연결
-    private final JdbcTemplate jdbcTemplate;
+    // 멤버 변수 선언
+    private final MemoRepository memoRepository;
 
-    public MemoService(JdbcTemplate jdbcTemplate){
-        this.jdbcTemplate=jdbcTemplate;
+    //생성자 주입(DI)
+    public MemoService(MemoRepository memoRepository) {
+        this.memoRepository = memoRepository;
     }
-
-    public MemoResponseDto creatMemo(@RequestBody MemoRequestDto memoRequestDto){
+    @Transactional(readOnly = true)
+    public MemoResponseDto createMemo(@RequestBody MemoRequestDto memoRequestDto) {
         // RequestDto -> Entity 변환
-        Memo newMemo= new Memo(memoRequestDto);
-
-        MemoRepository memoRepository = new MemoRepository(jdbcTemplate);
-        Memo saveMemo = memoRepository.save(newMemo);
+        Memo newMemo = new Memo(memoRequestDto);
+        Memo savedMemo = memoRepository.save(newMemo);
 
         // Entity -> ResponseDto 변환
-        MemoResponseDto memoResponseDto=new MemoResponseDto(saveMemo);
+        MemoResponseDto memoResponseDto = new MemoResponseDto(savedMemo);
 
-        return null;
+        return memoResponseDto;
     }
 
+    @Transactional(readOnly = true)
+    public Memo getMemoById(Long id){
+        return memoRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("선택한 id의 메모는 존재하지 않습니다."));
+    }
 
     public List<MemoResponseDto> getMemos() {
-       MemoRepository memoRepository = new MemoRepository(jdbcTemplate);
-       List<MemoResponseDto> memoResponseDtoList = memoRepository.findAll();
+        List<MemoResponseDto> memoResponseDtoList = memoRepository.findAll().stream()
+                .map(MemoResponseDto::new).toList();
         return memoResponseDtoList;
     }
 
+    @Transactional(readOnly = true)
+    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto memoRequestDto) {
 
-    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto memoRequestDto){
         // 해당 id의 메모가 존재하는지 확인
-        Memo foundMemo=findById(id);
+        Memo foundMemo = getMemoById(id);
 
         // 메모 내용 수정
-        if(foundMemo != null){
-            String sql="UPDATE memo SET username = ?, contents = ? WHERE id = ?";
-            jdbcTemplate.update(sql, memoRequestDto.getUsername(), memoRequestDto.getContents(), id);
-
-            return id;
-        } else{
-            throw new IllegalArgumentException("선택한 id의 메모는 존재하지 않습니다.");
-        }
+        foundMemo.update(memoRequestDto);
+        return id;
     }
+    @Transactional(readOnly = true)
+    public Long deleteMemo(@PathVariable Long id) {
 
-
-    public Long deleteMemo(@PathVariable Long id){
-        // 해당 id의 메모가 데이터베이스에 존재하는지 확인
-        Memo foundMemo=findById(id);
-
+        // 해당 id의 메모가 존재하는지 확인
+        Memo foundMemo = getMemoById(id);
         // 메모 내용 삭제
-        if(foundMemo != null){
-            String sql ="DELETE FROM memo WHERE id = ?";
-            jdbcTemplate.update(sql,id);
-
-            return id;
-        }else{
-            throw new IllegalArgumentException("선택한 id의 메모는 존재하지 않습니다.");
-        }
-
-    }
-
-    //특정 id의 메모 존재 여부 확인 공용메서드
-    private Memo findById(Long id){
-        // DB 조회
-        String sql ="SELECT * FROM memo where id = ?";
-
-        return jdbcTemplate.query(sql, resultSet ->{
-            if(resultSet.next()){
-                Memo memo = new Memo();
-                memo.setUsername(resultSet.getString("username"));
-                memo.setContents(resultSet.getString("contents"));
-                return memo;
-            }else{
-                return null;
-            }
-        },id);
-
+        memoRepository.delete(foundMemo);
+        return id;
 
     }
 
 }
+
